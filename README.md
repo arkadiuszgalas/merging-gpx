7dc9b1e628235027c95638375d56783c2b47872f
Arkadiusz Galas <arkadiusz.galas@gmail.com>
2026-03-21 13:37:47 +0100

    First working program version. Added: validation if there is no duplicates in .gpx input files output filename as place_place_yyyyddmm_timestamp.gpx

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # merging-gpx
 Script reads all .gpx files in input directory.
-Scripts reads minimum and maximum timestamp in every file found between tags
-<time> and </time>.
+Scripts finds minimum and maximum timestamp in every file. Next validation is done i.e.
+checking if there are no overlaps/duplicates between files. Files are sorted
+in ascending way i.e. from oldest to newest.
+If validation is correct script merges all gpx files into one (that can be
+uploaded e.g. to Strava).
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import datetime
 import os
+from pathlib import Path
 
-# Script creates list containing of: filename, minimum timestamp, maximum timestamp
-# Script sorts list by minimum timestamp (found within .gpx file)
+# Ideas for improvements:
+# 1. Checking if input directory is not empty
+# 2. Checking if gpx filenames have correct naming conventions (places and spaces)
+# 3. Moving creating output file to function
+# 4. Changing all operation on files and directories to Path objects
+# 5. Changing absolute paths into relative
 
 def find_minimum_date(gpx_input_list):
    minimum_ts = datetime.datetime(2040, 12, 31)
@@ -41,6 +46,27 @@ def bubble_sort(input_list):
          if input_list[j][0] > input_list[j + 1][0]:
             input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
 
+def set_filename(input_list,gpx_input_list):
+   n = len(input_list)
+   start_place = input_list[0][2][0:input_list[0][2].find("_")]
+   end_place_first_sep = input_list[n - 1][2].find("_")
+   end_place_second_sep = input_list[n - 1][2][end_place_first_sep + 1:].find("_")
+   end_place = input_list[n - 1][2][end_place_first_sep + 1:end_place_first_sep + end_place_second_sep + 1]
+   filename_out = ''
+   for val in gpx_input_list:
+      if val.find("<metadata>") != -1 and val.find("</metadata>") != -1:
+         date_trip = val[val.find("<time>") + 6:val.find("time") + 15]
+         date_trip = date_trip.translate(str.maketrans('', '', '-'))
+   datetime_now = str(datetime.datetime.now())
+   datetime_now = datetime_now.replace('-','_')
+   datetime_now = datetime_now.replace(':','_')
+   datetime_now = datetime_now.replace(' ','_')
+   datetime_now = datetime_now.replace('.','_')[:-3]
+
+   # creating final version of filename
+   filename_out = start_place + '_' + end_place + '_' + date_trip + '_' + datetime_now
+   return filename_out
+
 # List gpx_times contains minimum timestamp, maximum timestamp and filename found in input directory
 gpx_times = []
 
@@ -64,8 +90,49 @@ for file_gpx in files_list:
 bubble_sort(gpx_times)
 
 gpx_valid = True
+
 # Validating if there is overlaps in gpx files
 for i in range(len(gpx_times) - 1):
    if gpx_times[i][1] >= gpx_times[i+1][0]:
       gpx_valid = False
-print(f"VAlidation status: {gpx_valid}")
+
+print(f"Validation status: {gpx_valid}")
+if gpx_valid:
+   # setting output file name
+   filename_output = set_filename(gpx_times,gpx_data_list)
+
+   # checking if output directory exists
+   output_dir = Path('/Users/arkadiuszgalas/Documents/python/merging-gpx/output')
+   if not output_dir.exists():
+      os.makedirs('/Users/arkadiuszgalas/Documents/python/merging-gpx/output')
+
+   # creating output .gpx file in output directory
+   dir_name_output = os.path.join(dir_name, 'output')
+   gpx_file_path_out = os.path.join(dir_name_output, filename_output)
+   outputGpxFile = open(gpx_file_path_out, 'w', encoding = 'utf8')
+
+   for l in range(len(gpx_times)):
+      gpx_file_path_w = os.path.join(dir_name_input,gpx_times[l][2])
+      gpx_file_w = open(gpx_file_path_w, "r", encoding = "utf8")
+      gpx_data_w = gpx_file_w.read()
+      gpx_data_list_w = gpx_data_w.split("\n")
+      gpx_file_w.close()
+
+      if l == 0:
+         for val_w in gpx_data_list_w:
+            if val_w.find("</trkseg>") == -1 and val_w.find("</trk>") == -1 and val_w.find("</gpx>") == -1:
+               outputGpxFile.write(val_w)
+               outputGpxFile.write("\n")
+      else:
+         for val_w in gpx_data_list_w:
+            if val_w.find("<trkpt") != -1:
+               outputGpxFile.write(val_w)
+               outputGpxFile.write("\n")
+   outputGpxFile.write("    </trkseg>")
+   outputGpxFile.write("\n")
+   outputGpxFile.write("  </trk>")
+   outputGpxFile.write("\n")
+   outputGpxFile.write("</gpx>")
+   outputGpxFile.write("\n")
+else:
+   print("Validation of gpx filed failed.")
